83c75755575f6090fdddea1d03853ded94ef909c
manu0bhardwaj <160593264+manu0bhardwaj@users.noreply.github.com>
2024-04-24 22:58:24 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const Razorpay = require("razorpay");
+const cors = require("cors");
+const crypto = require("crypto");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.post("/order", async (req, res) => {
+  try {
+    const razorpay = new Razorpay({
+      key_id: "rzp_test_gYiq57okl7muXo",
+      key_secret: "wg4q0kazhMfnfNmUEpg8N9xG",
+    });
+
+    const options = req.body;
+    const order = await razorpay.orders.create(options);
+
+    if (!order) {
+      return res.status(500).send("Error");
+    }
+
+    res.json(order);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error");
+  }
+});
+
+app.post("/order/validate", async (req, res) => {
+  const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+    req.body;
+
+  const sha = crypto.createHmac("sha256","wg4q0kazhMfnfNmUEpg8N9xG" );
+  //order_id + "|" + razorpay_payment_id
+  sha.update(`${razorpay_order_id}|${razorpay_payment_id}`);
+  const digest = sha.digest("hex");
+  if (digest !== razorpay_signature) {
+    return res.status(400).json({ msg: "Transaction is not legit!" });
+  }
+
+  res.json({
+    msg: "success",
+    orderId: razorpay_order_id,
+    paymentId: razorpay_payment_id,
+  });
+});
+
+app.listen(5000, () => {
+  console.log("Listening on port", 5000);
+});
